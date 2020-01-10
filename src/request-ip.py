f63e31321bbd994b6d99384e061dc67b62b5e48d
Doc Duncan <triplerockmentality@icloud.com>
2020-01-09 20:31:34 -0500

    DMD (D) S3 properties drawn from env. vars

--- src/request-ip.py
+++ src/request-ip.py
@@ -1,8 +1,8 @@
 import boto3
 import json
+import os
 from requests import get
 
-# TODO extract bucket name and key to env vars
 # TODO run on cron or continuously
 
 s3_client = boto3.client('s3')
@@ -13,5 +13,7 @@ public_ip = get('https://api.ipify.org').text
 ip_output_object['public-ip-address'] = public_ip
 ip_output_object_bytes = str.encode(json.dumps(ip_output_object))
 
-s3_client.put_object(Body=ip_output_object_bytes, Bucket='dynamic-ip', Key='current-public-ip.json')
+s3_bucket_name = os.getenv('S3_BUCKET_NAME')
+s3_file_name = os.getenv('S3_FILE_NAME')
 
+s3_client.put_object(Body=ip_output_object_bytes, Bucket=s3_bucket_name, Key=s3_file_name)
