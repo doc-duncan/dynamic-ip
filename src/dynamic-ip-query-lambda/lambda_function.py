d894c62503f3187f8dd27bcade26df6e1d98dfbb
Doc Duncan <triplerockmentality@icloud.com>
2020-01-30 23:42:32 -0500

    DMD (D) lambda to get uploaded ip info from s3

---
+++ src/dynamic-ip-query-lambda/lambda_function.py
@@ -0,0 +1,16 @@
+import boto3
+import json
+
+def lambda_handler(event, context):
+    s3_bucket_name = event['S3_BUCKET_NAME']
+    s3_base_file_name = event['S3_BASE_FILE_NAME']
+    device_identifier = event['DEVICE_IDENTIFIER']
+    
+    s3_key = device_identifier + '-' + s3_base_file_name
+    s3_client = boto3.client('s3')
+    ip_file = s3_client.get_object(Bucket=s3_bucket_name, Key=s3_key)
+    
+    return {
+        'statusCode': 200,
+        'body': json.loads(ip_file['Body'].read().decode('utf-8'))
+    }
