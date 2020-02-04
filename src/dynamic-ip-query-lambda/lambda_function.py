184f0ebccfcdf9295c099268e1da5b309788b075
Doc Duncan <triplerockmentality@icloud.com>
2020-02-03 22:01:03 -0500

    DMD (D) documentation

--- src/dynamic-ip-query-lambda/lambda_function.py
+++ src/dynamic-ip-query-lambda/lambda_function.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 
+# builds S3 object key from input, fetches object and returns corresponding IP info
 def lambda_handler(event, context):
     s3_bucket_name = event['S3_BUCKET_NAME']
     s3_base_file_name = event['S3_BASE_FILE_NAME']
@@ -9,7 +10,7 @@ def lambda_handler(event, context):
     s3_key = device_identifier + '-' + s3_base_file_name
     s3_client = boto3.client('s3')
     ip_file = s3_client.get_object(Bucket=s3_bucket_name, Key=s3_key)
-    
+
     return {
         'statusCode': 200,
         'body': json.loads(ip_file['Body'].read().decode('utf-8'))
