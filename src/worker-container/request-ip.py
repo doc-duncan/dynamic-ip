040306a8acf070043597307d2bff14db30f6b886
Doc Duncan <triplerockmentality@icloud.com>
2020-02-09 19:59:58 -0500

    DMD (D) removed S3_BUCKET_NAME and S3_BASE_FILE_NAME ENV vars

--- src/worker-container/request-ip.py
+++ src/worker-container/request-ip.py
@@ -9,14 +9,12 @@ from requests import get
 # TODO run operation under stricter AWS permissions : DONE
 # TODO run as api through api gateway
 # TODO put machine id on payload : DONE
-
-# env vars set in input env_file with docker run
-s3_bucket_name = os.getenv('S3_BUCKET_NAME')
-s3_base_file_name = os.getenv('S3_BASE_FILE_NAME')
-device_identifier = os.getenv('DEVICE_IDENTIFIER')
-
 s3_client = boto3.client('s3')
 ip_output_object = {}
+
+s3_bucket_name = 'dynamic-ip'
+device_identifier = os.getenv('DEVICE_IDENTIFIER')
+s3_key = device_identifier + '.json'
 
 # get public ip and encode for s3 put
 public_ip = get('https://api.ipify.org').text
@@ -24,5 +22,4 @@ ip_output_object['public-ip-address'] = public_ip
 ip_output_object['device-identifier'] = device_identifier
 ip_output_object_bytes = str.encode(json.dumps(ip_output_object))
 
-s3_key = device_identifier + '-' + s3_base_file_name
 s3_client.put_object(Body=ip_output_object_bytes, Bucket=s3_bucket_name, Key=s3_key)
