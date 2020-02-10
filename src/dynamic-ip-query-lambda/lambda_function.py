6a34f5e101da3ab92cc08e717af7ff24708d1d18
Doc Duncan <triplerockmentality@icloud.com>
2020-02-09 20:06:33 -0500

    DMD (D) only requiring DEVICE_IDENTIFIER as ENV var

--- src/dynamic-ip-query-lambda/lambda_function.py
+++ src/dynamic-ip-query-lambda/lambda_function.py
@@ -3,12 +3,12 @@ import json
 
 # builds S3 object key from input, fetches object and returns corresponding IP info
 def lambda_handler(event, context):
-    s3_bucket_name = event['S3_BUCKET_NAME']
-    s3_base_file_name = event['S3_BASE_FILE_NAME']
-    device_identifier = event['DEVICE_IDENTIFIER']
-    
-    s3_key = device_identifier + '-' + s3_base_file_name
     s3_client = boto3.client('s3')
+
+    s3_bucket_name = 'dynamic-ip'
+    device_identifier = event['DEVICE_IDENTIFIER']
+    s3_key = device_identifier + '.json'
+    
     ip_file = s3_client.get_object(Bucket=s3_bucket_name, Key=s3_key)
 
     return {
