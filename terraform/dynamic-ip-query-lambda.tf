96a89f35d6d1c9bd03fede5988343f3fb1aea5b6
Doc Duncan <triplerockmentality@icloud.com>
2020-02-23 21:11:50 -0500

    DMD (D) added permissions for new api gateway method to trigger lambda

--- terraform/dynamic-ip-query-lambda.tf
+++ terraform/dynamic-ip-query-lambda.tf
@@ -1,3 +1,13 @@
+resource "aws_lambda_permission" "dynamic-ip-query-api-to-lambda-bridge" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.dynamic-ip-query.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "${aws_api_gateway_rest_api.dyanmic-ip-query-api.execution_arn}/*/${aws_api_gateway_method.dynamic-ip-query-api-get-method.http_method}${aws_api_gateway_resource.dynamic-ip-query-api-resource.path}"
+}
+
 resource "aws_lambda_function" "dynamic-ip-query" {
   filename      = "../src/dynamic-ip-query-lambda/function.zip"
   function_name = "dynamic-ip-query"
